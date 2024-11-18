0dc57134b54fe16351afd210b5258d6b50f1efb4
allan <allan.gtm92@gmail.com>
2024-11-18 17:40:40 +0545

    Finished question 6

---
+++ question_six.dart
@@ -0,0 +1,11 @@
+// Write a dart program to generate multiplication tables of 5.
+
+void multipleOfFive() {
+  for (int i = 1; i <= 10; i++) {
+    print("5 X $i = ${i * 5}");
+  }
+}
+
+void main() {
+  multipleOfFive();
+}
